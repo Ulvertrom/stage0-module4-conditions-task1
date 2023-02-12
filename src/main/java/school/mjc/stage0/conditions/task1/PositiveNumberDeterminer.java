7d5eed8e17bc9338a91144423bc1d199a0b06daa
ulvertrom <ulvertrom@gmail.com>
2023-02-12 21:24:44 +0400

    5 progs added

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        boolean condition = (numberToBeDetermined > 0);
+        if (condition){
+            System.out.println(condition);
+        } else {
+            System.out.println(condition);
+        }
+
     }
 }
